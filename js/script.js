ba711f2b31cbdaed3eb16088bd6951a5259fc730
devshimul <shimul.bisd@gmail.com>
2024-12-11 21:59:37 +0600

    dom traversing

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const liCollection = document.getElementsByTagName("li");
+for (const li of liCollection) {
+  li.style.color = "#fff";
+  li.style.backgroundColor = "green";
+  li.style.marginBottom = "5px";
+  li.style.padding = "10px";
+}
